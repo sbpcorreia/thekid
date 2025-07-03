19087b6ecb22e4d929638963b098a7007c2cfd1c
Pedro Correia <pcorreia@solid-bridge.pt>
2025-07-03 17:26:41 +0100

    Fix: Carrinhos a aparecer quando tarefa removida

--- app/Models/CartsModel.php
+++ app/Models/CartsModel.php
@@ -58,7 +58,7 @@ class CartsModel extends Model {
         }    
         $builder->join("u_kidtask", "u_kidtask.carrinho=u_kidcart.codigo", "left");
         $builder->groupStart();
-        $builder->whereIn("u_kidtask.estado", array(9));
+        $builder->whereIn("u_kidtask.estado", array(9,5));
         $builder->orWhere("u_kidtask.u_kidtaskstamp IS NULL", "", false);
         $builder->groupEnd();
         $query = $builder->get($pageSize, ($pageSize) * ($page-1));
@@ -71,7 +71,7 @@ class CartsModel extends Model {
         $builder->join("u_kidtask", "u_kidtask.carrinho=u_kidcart.codigo", "left");
         $builder->where("u_kidcart.codigo", $cartCode);
         $builder->groupStart();
-        $builder->whereIn("u_kidtask.estado", array(9));
+        $builder->whereIn("u_kidtask.estado", array(9,5));
         $builder->orWhere("u_kidtask.u_kidtaskstamp IS NULL", "", false);
         $builder->groupEnd();
         $query = $builder->get();
