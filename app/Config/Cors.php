d8f74eedab984146475e8dad3bbe04d6b689e023
Pedro Correia <pcorreia@solid-bridge.pt>
2025-07-31 18:17:24 +0100

    Fix: Cors

--- app/Config/Cors.php
+++ app/Config/Cors.php
@@ -68,7 +68,7 @@ class Cors extends BaseConfig
          *
          * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Headers
          */
-        'allowedHeaders' => [],
+        'allowedHeaders' => ['Authorization', 'Content-Type'],
 
         /**
          * Set headers to expose.
@@ -93,7 +93,7 @@ class Cors extends BaseConfig
          *
          * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Methods
          */
-        'allowedMethods' => ['GET', 'POST'],
+        'allowedMethods' => ['GET', 'POST', 'PUT', 'DELETE'],
 
         /**
          * Set how many seconds the results of a preflight request can be cached.
