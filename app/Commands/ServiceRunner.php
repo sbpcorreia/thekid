9a6a7f86a557634831cb544030eb2dc193e2da29
Pedro Correia <pcorreia@solid-bridge.pt>
2025-12-11 09:06:46 +0000

    Chg: Alterado o intervalo de reinício do comando

--- app/Commands/ServiceRunner.php
+++ app/Commands/ServiceRunner.php
@@ -14,7 +14,7 @@ class ServiceRunner extends BaseCommand {
 
     // --- NOVO ---
     // Defina o intervalo de reinício em segundos (Ex: 6 horas)
-    const RESTART_INTERVAL = 4 * 3600; 
+    const RESTART_INTERVAL = 2 * 3600;
 
     public function run(array $params)
     {
